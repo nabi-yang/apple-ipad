0ed933c6ff83b6ae8727a6edfd534d44f5b09089
nabi-yang <sunae894@naver.com>
2023-04-11 17:45:43 +0900

    Accessory,trade

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,5 @@
+import ipads from '../data/ipads.js'
+
 //장바구니
 const basketStarterEl = document.querySelector('header .basket-starter')
 const basketEl = basketStarterEl.querySelector('.basket')
@@ -100,3 +102,25 @@ pauseBtn.addEventListener('click',function(){
   playBtn.classList.remove('hide')
   pauseBtn.classList.add('hide')
 });
+
+
+// compare
+const itemsEl=document.querySelector('section.compare .items')
+ipads.forEach(function (ipad){
+  const itemEl = document.createElement('div')
+  itemEl.classList.add('item')
+  itemEl.innerHTML =/*html*/`
+  <div class="thumbnail"> 
+    <img src="${ipad.thumbnail}" alt="${ipad.name}" />
+  </div>
+  <ul class="colors">
+
+  </ul>
+  <h3 class="name">${ipad.name}</h3>
+  <p class="tagline">${ipad.tagline}</p>
+  <p class="price">${ipad.price}</p>
+  <button class="btn">구입하기</button>
+  <a href="${ipad.url}" class="link">더 알아보기</a>
+  `
+  itemsEl.append(itemEl)
+})
